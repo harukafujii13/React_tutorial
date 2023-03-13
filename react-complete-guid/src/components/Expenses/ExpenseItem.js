47c2482ac2a74712c7701f21cbbff12244066e9a
Haruka <hfujii1013@gmail.com>
2023-03-12 17:53:18 -0700

    added till section 4-55

--- react-complete-guid/src/components/Expenses/ExpenseItem.js
+++ react-complete-guid/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"; //install css file
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); //it must be called inside component function
+  // the first element is the current state value
+  // the second element is a function for updating that
+
+  console.log("ExpenseItem evaluated by React");
+
   const clickHandler = () => {
-    console.log("Clicked!!");
+    setTitle("Updated!");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
