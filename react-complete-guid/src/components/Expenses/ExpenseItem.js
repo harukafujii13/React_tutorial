9865b3c61828fda65586f3ab5fd138b2c0164d7b
Haruka <hfujii1013@gmail.com>
2023-03-13 18:48:44 -0700

    Done section 4

--- react-complete-guid/src/components/Expenses/ExpenseItem.js
+++ react-complete-guid/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"; //install css file
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title); //it must be called inside component function
+  // const [title, setTitle] = useState(props.title); //it must be called inside component function
   // the first element is the current state value
   // the second element is a function for updating that
 
-  console.log("ExpenseItem evaluated by React");
+  // console.log("ExpenseItem evaluated by React");
 
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
+      {/* <button onClick={clickHandler}>Change title</button> */}
     </Card> //can only have one root element
   );
 };
