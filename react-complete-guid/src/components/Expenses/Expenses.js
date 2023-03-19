1c4623f215cd026e991ab6bd2e57f83c390c2e67
Haruka <hfujii1013@gmail.com>
2023-03-18 19:19:20 -0700

    added section 5-71

--- react-complete-guid/src/components/Expenses/Expenses.js
+++ react-complete-guid/src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+
 import Card from "../UI/Card";
-import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -21,6 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filterExpenses} />
         <ExpensesList items={filterExpenses} />
       </Card>
     </div>
--- react-complete-guid/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guid/src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enterTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
