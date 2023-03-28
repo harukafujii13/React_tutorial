27ed6a7364dd9b1f3af4acf9f20d760cb43f4eb9
Haruka <hfujii1013@gmail.com>
2023-03-28 16:49:38 -0700

    added 01-starting-project as a re-review

--- Section8_Project/src/assets/components/UI/Card.jsx
+++ Section8_Project/src/assets/components/UI/Card.jsx
@@ -7,3 +7,11 @@ const Card = (props) => {
 };
 
 export default Card;
+
+//the className prop can be used to add any custom classes to the element that is being styled. 
+//This is what is happening in the <Card className={classes.input}> code, where a custom class named "input" is being added to the Card component.
+
+//So, the <div className={${classes.card} ${props.className}}> and <Card className={classes.input}> 
+//are related in that the className prop is used to apply CSS classes to both elements, 
+//with the Card component defining the overall styling for the card and the custom class 
+//applied to it further customizing its appearance.
